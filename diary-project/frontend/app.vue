2072a31577fbce9c92630c78510f9678f9ef89fc
Taiga2024 <taiga.shimizu@bizmates.co.jp>
2024-07-23 16:22:25 +0900

    create-GraphQL-schema

--- diary-project/frontend/app.vue
+++ diary-project/frontend/app.vue
@@ -1,6 +1,38 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Hello Nuxt3</h1>
+    <h2>{{fetchData}}</h2>
   </div>
 </template>
+
+<script setup lang="ts">
+  import { ref } from "vue";
+  import { GraphQLClient } from 'graphql-request';
+
+  const fetchData=ref();
+
+  const runtimeConfig = useRuntimeConfig();
+
+  console.log(runtimeConfig.public.apiUrl)
+  const client = new GraphQLClient(runtimeConfig.public.apiUrl);
+
+  const query = `{
+  user(id: 1) {
+    id
+    name
+    email
+  }
+}
+`;
+
+  async function fetchUserData() {
+    try {
+      const data = await client.request(query);
+      fetchData.value=data.user
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  fetchUserData();
+</script>
